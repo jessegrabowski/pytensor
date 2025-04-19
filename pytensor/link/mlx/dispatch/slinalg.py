70725d00d87c00c9bdd78b0453e2fc38539fe950
jessegrabowski <jessegrabowski@gmail.com>
2025-04-18 21:17:10 -0500

    mlx solve dispatch

---
+++ pytensor/link/mlx/dispatch/slinalg.py
@@ -0,0 +1,24 @@
+import warnings
+
+import mlx.core as mx
+
+from pytensor.link.mlx.dispatch.basic import mlx_funcify
+from pytensor.tensor.slinalg import Solve
+
+
+@mlx_funcify.register(Solve)
+def mlx_funcify_Solve(op, node, **kwargs):
+    assume_a = op.assume_a
+
+    if assume_a != "gen":
+        warnings.warn(
+            f"MLX solve does not support assume_a={op.assume_a}. Defaulting to assume_a='gen'.\n"
+            f"If appropriate, you may want to set assume_a to one of 'sym', 'pos', 'her' or 'tridiagonal' to improve performance.",
+            UserWarning,
+        )
+
+    def solve(a, b):
+        # MLX only supports solve on CPU
+        return mx.linalg.solve(a, b, stream=mx.cpu)
+
+    return solve
---
+++ tests/link/mlx/test_slinalg.py
@@ -0,0 +1,26 @@
+import numpy as np
+
+import pytensor.tensor as pt
+from pytensor import config
+from tests.link.mlx.test_basic import compare_mlx_and_py, mlx_mode
+
+
+def test_mlx_solve():
+    rng = np.random.default_rng()
+
+    A = pt.tensor("A", shape=(5, 5))
+    b = pt.tensor("B", shape=(5, 5))
+
+    out = pt.linalg.solve(A, b, b_ndim=2)
+
+    A_val = rng.normal(size=(5, 5)).astype(config.floatX)
+    b_val = rng.normal(size=(5, 5)).astype(config.floatX)
+
+    compare_mlx_and_py(
+        [A, b],
+        [out],
+        [A_val, b_val],
+        # mlx complains about useless vmap (when there are no batch dims), so we need to include
+        # local_remove_useless_blockwise rewrite for this test
+        mlx_mode=mlx_mode.including("blockwise"),
+    )
